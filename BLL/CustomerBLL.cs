96bb41818b53c67e54c4862dd3222322b74ff41f
minhchuong32 <chuongminh3225@gmail.com>
2025-09-01 15:38:35 +0700

    create customer bll

--- BLL/CustomerBLL.cs
+++ BLL/CustomerBLL.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using CustomerModuleManager.DAL;
+using CustomerModuleWinForms.Models;
 
 namespace CustomerModuleManager.BLL
 {
     class CustomerBLL
     {
+        CustomerDAL dal = new CustomerDAL();
+
+        public DataTable GetCustomers() => dal.GetAllCustomers();
+        public int Add(Customer c) => dal.AddCustomer(c);
+        public int Update(Customer c) => dal.UpdateCustomer(c);
+        public int Delete(int id) => dal.DeleteCustomer(id);
     }
 }
